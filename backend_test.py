fb45d24e02841121d0cd6ed7e296cdb329ee7e9a
AbogiC <venturaabedbogichrist314@gmail.com>
2025-09-19 08:16:32 +0700

    change environment to local

--- backend_test.py
+++ backend_test.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import time
 
 class ABCMusicLibraryTester:
-    def __init__(self, base_url="https://musicsheets-1.preview.emergentagent.com/api"):
+    def __init__(self, base_url="http://127.0.0.1:8000/api"):
         self.base_url = base_url
         self.token = None
         self.user_id = None
